87abbaf18a81f81de26def265bea2f94d14142a3
Bernard113-svg <bernardsanluis510@gmail.com>
2026-08-23 14:37:32 +0800

    Add files via upload

---
+++ C# Compiler.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 20;                    
+        char grade = 'J';                
+        string name = "Bernard";         
+        bool isGraduated = true;        
+        double price = 18.99;          
+
+        Console.WriteLine(age);
+        Console.WriteLine(grade);
+        Console.WriteLine(name);
+        Console.WriteLine(isGraduated);
+        Console.WriteLine(price);
+    }
+}
